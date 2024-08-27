f9232c09f60345c46e91ec2ec3578d6f912816de
Anderson Martins <atmmoreira.rj@gmail.com>
2024-08-27 16:09:26 -0300

    feature: starting dynamic content for homepage

---
+++ src/archive.php
@@ -0,0 +1,100 @@
+<?php get_header(); ?>
+
+<section class="post-page my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-2 d-none d-md-block">
+        <!-- Post preview-->
+        <h5 class="text-uppercase">Categories</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">CSS3</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">Vanilla JS</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">jQuery</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">React</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">PHP</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">WordPress</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">MySQL</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">SASS</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">TailwindCSS</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">Vite</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">Node</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">Laravel</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">TDD</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">OOP</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">GraphQL</a>
+          </li>
+          <li class="nav-item mb-2">
+            <i class="bi bi-folder"></i>
+            <a href="#" class="text-muted text-decoration-none">Jest / Cypress</a>
+          </li>
+        </ul>
+      </div> <!-- List of Categories -->
+      <div class="col-10">
+        <h2 class="text-uppercase"> <?php the_archive_title(); ?> </h2>
+        <div class="row">
+          <?php while(have_posts()) { the_post();  ?>          
+            <div class="col-12 col-md-4 my-3">
+              <small>By <?php the_author_posts_link(); ?> &middot; <?php echo get_the_date('M j, Y'); ?> in 
+                <span class="category">                  
+                  <?= preg_replace('/<a /', '<a class="text-danger fw-bold text-decoration-none"', get_the_category_list( ', ' )); ?>
+                </span>
+              </small>
+              <div class="articles__title text-uppercase lh-sm">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+              </div>
+              <?php the_excerpt(); ?>
+            </div>
+          <?php } ?>
+        </div>
+        <div class="text-center">
+          <?= paginate_links(array('prev_text' => '«', 'next_text' => '»')); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
--- src/front-page.php
+++ src/front-page.php
@@ -104,63 +104,28 @@
 
 <section class="articles">
   <div class="container py-4">
-    <h2 class="text-uppercase text-center">Articles</h2>
+    <h2 class="text-uppercase text-center">Recent Articles</h2>
     <div class="divider"></div>
-    <small class="d-flex justify-content-center mt-3"><a href="#" class="link">all posts</a></small>
+    <small class="d-flex justify-content-center mt-3"><a href="<?= site_url('blog'); ?>" class="link">all posts</a></small>
 
     <div class="row">
-      <div class="col-12 col-md-3 my-3">
-        <small>Published Apr 29 2024 in
-          <span class="category">CSS3</span></small>
-        <div class="articles__title text-uppercase lh-sm">
-          <a href="#">The CSS stepped value math functions are now in Baseline
-            2024</a>
+      <?php
+      $homePosts = new WP_Query(array('posts_per_page' => 4));
+      while ($homePosts->have_posts()) {
+        $homePosts->the_post(); ?>
+
+        <div class="col-12 col-md-3 my-3">
+          <small> <?php echo get_the_date('M j, Y'); ?> in
+            <span class="category">
+              <?= preg_replace('/<a /', '<a class="text-danger fw-bold text-decoration-none"', get_the_category_list(', ')); ?>
+            </span>
+          </small>
+          <div class="articles__title text-uppercase lh-sm">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </div>
+          <?php the_excerpt(); ?>
         </div>
-        <p>
-          In CSS Grid, the “grid-template-columns” and “grid-template-rows”
-          properties allow you to define line names and track sizing of grid
-          columns and rows, respectively.
-        </p>
-      </div>
-      <div class="col-12 col-md-3 my-3">
-        <small>Published Apr 29 2024 in
-          <span class="category">CSS3</span></small>
-        <div class="articles__title text-uppercase lh-sm">
-          <a href="#">The CSS stepped value math functions are now in Baseline
-            2024</a>
-        </div>
-        <p>
-          In CSS Grid, the “grid-template-columns” and “grid-template-rows”
-          properties allow you to define line names and track sizing of grid
-          columns and rows, respectively.
-        </p>
-      </div>
-      <div class="col-12 col-md-3 my-3">
-        <small>Published Apr 29 2024 in
-          <span class="category">CSS3</span></small>
-        <div class="articles__title text-uppercase lh-sm">
-          <a href="#">The CSS stepped value math functions are now in Baseline
-            2024</a>
-        </div>
-        <p>
-          In CSS Grid, the “grid-template-columns” and “grid-template-rows”
-          properties allow you to define line names and track sizing of grid
-          columns and rows, respectively.
-        </p>
-      </div>
-      <div class="col-12 col-md-3 my-3">
-        <small>Published Apr 29 2024 in
-          <span class="category">CSS3</span></small>
-        <div class="articles__title text-uppercase lh-sm">
-          <a href="#">The CSS stepped value math functions are now in Baseline
-            2024</a>
-        </div>
-        <p>
-          In CSS Grid, the “grid-template-columns” and “grid-template-rows”
-          properties allow you to define line names and track sizing of grid
-          columns and rows, respectively.
-        </p>
-      </div>
+      <?php } wp_reset_postdata(); ?>
     </div>
   </div>
 </section>
