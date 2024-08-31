4bb8e9162ef04bc467c535ae6f0fa00f08e80bf1
Anderson Martins <atmmoreira.rj@gmail.com>
2024-08-31 14:10:24 -0300

    feature: adding new images and edit the content for front-page.php

--- src/front-page.php
+++ src/front-page.php
@@ -1,113 +1,13 @@
 <?php get_header(); ?>
 
-<section class="container my-5">
-  <div class="row">
-    <div class="col-12 col-lg-4 col-xl-3 text-center">
-      <img src="./assets/images/photo.png" alt="" />
-      <div class="buttons m-auto">
-        <a href="#"><img src="./assets/images/flag-brazil.svg" alt="" /></a>
-        <a href="#"><img src="./assets/images/flag-united-states.svg" alt="" /></a>
-        RESUME
-      </div>
-      <div class="m-auto my-5 d-flex justify-content-evenly">
-        <a href="#"><img src="./assets/images/github-icon.svg" alt="" /></a>
-        <a href="#"><img src="./assets/images/linkedin-icon.svg" alt="" /></a>
-      </div>
-    </div>
-    <div class="col-12 col-lg-8 col-xl-9">
-      <small class="fw-bold">Hello, I'm</small>
-      <div class="mb-4">
-        <h1>Anderson Martins</h1>
-        <h3>Frontend Software Developer & WordPress Software Engineer</h3>
-      </div>
-      <p>
-        Frontend Software Developer based in Rio de Janeiro – Brazil, with
-        over 6 years of experience in developing and implementing scalable
-        solutions using JavaScript technologies including jQuery, React.js,
-        and Next.js. My expertise focuses on performance optimization and
-        code maintainability. I have extensive experience with Responsive
-        and Adaptative Design using Flexbox, Grid Layout, Media Queries,
-        HTML5, and CSS3 (including SASS, Bootstrap, and TailwindCSS). My
-        skills encompass designing and consuming Restful APIs, emphasizing
-        efficient data handling and integration. I am adept at creating
-        reusable and modular components, and am passionate about staying
-        updated with the latest trends in React.js Development.
-      </p>
-
-      <p>
-        Also have experience with WordPress, and expertise with of WP
-        Themes, WP Plugins, Core Functions, Taxonomies, Custom Post Types,
-        Advanced Custom Fields, Hooks (Actions and Filters), WP Rest API,
-        and builders as Elementor, Beaver Builder, Divi, WP Bakery, and
-        Gutenberg Custom Block Types. Additionally, I have a foundational
-        understanding of MERN (MongoDB, Express.js, React.js, Node.js),
-        TypeScript, Restful APIs, Microservices, Jest, Docker, GraphQL,
-        Nest.js, Sequelize ORM, Design Patterns and Test-Driven Development.
-      </p>
-    </div>
-  </div>
-</section>
-
-<section class="technical-skills">
-  <div class="container py-4">
-    <h2 class="text-uppercase text-center">Technical Skills</h2>
-    <div class="divider"></div>
-
-    <div class="row my-5">
-      <div class="col-12 col-md-3">
-        <div class="technical-skills__title">Web UI Designer</div>
-        <ul class="list-unstyled">
-          <li class="technical-skills__items">
-            Websites / Web App's / E-commerce
-          </li>
-          <li class="technical-skills__items">Prototyping / Wireframing</li>
-          <li class="technical-skills__items">Figma / Illustrator</li>
-          <li class="technical-skills__items">
-            Web and Mobile App's Design
-          </li>
-        </ul>
-      </div>
-      <div class="col-12 col-md-3">
-        <div class="technical-skills__title">Frontend Developer</div>
-        <ul class="list-unstyled">
-          <li class="technical-skills__items">HTML5 / CSS3 / SASS</li>
-          <li class="technical-skills__items">JavaScript / React</li>
-          <li class="technical-skills__items">Bootstrap / TailwindCSS</li>
-          <li class="technical-skills__items">PHP / TypeScript / Node</li>
-        </ul>
-      </div>
-      <div class="col-12 col-md-3">
-        <div class="technical-skills__title">WordPress Developer</div>
-        <ul class="list-unstyled">
-          <li class="technical-skills__items">Custom Plugins and Themes</li>
-          <li class="technical-skills__items">
-            ACF / CTP / Gutenberg Blocks
-          </li>
-          <li class="technical-skills__items">WordPress Builders</li>
-          <li class="technical-skills__items">RESTfull API Development</li>
-        </ul>
-      </div>
-      <div class="col-12 col-md-3">
-        <div class="technical-skills__title">Others</div>
-        <ul class="list-unstyled">
-          <li class="technical-skills__items">MySQL and MongoDB</li>
-          <li class="technical-skills__items">Git / GitHub / Bitbucket</li>
-          <li class="technical-skills__items">
-            MVC Architecture / TDD / OOP
-          </li>
-          <li class="technical-skills__items">ClickUP / JIRA / Zapier</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</section>
+<?php the_content(); ?>
 
 <section class="articles">
   <div class="container py-4">
-    <h2 class="text-uppercase text-center">Recent Articles</h2>
-    <div class="divider"></div>
+    <!-- <h2 class="text-uppercase text-center">Recent Articles</h2>
+    <div class="divider"></div> 
     <small class="d-flex justify-content-center mt-3"><a href="<?= site_url('/blog'); ?>" class="link">all posts</a></small>
-
+-->
     <div class="row">
       <?php
         $homePosts = new WP_Query(array(
